9c23e356fc6bd84fdb5f40d4da83c2315e61499f
unknown <jersonsantana1126@gmail.com>
2026-09-14 09:14:12 -0500

    Ejercicio Clase Descuento

--- POO/Descuento/main.py
+++ POO/Descuento/main.py
@@ -3,10 +3,10 @@ from producto import Producto
 def main():
     
     producto1 = Producto(1425, "Tampico", 27, 3000)
-    print(f"El descuento del producto de: {producto1.descuento()}")
+    print(f"El descuento del producto: {producto1.nombre}, es de: {producto1.descuento()}")
     
     producto2 = Producto(7483, "Aloha", 53, 4500)
-    print(f"El descuento del producto de: {producto2.descuento()}")
+    print(f"El descuento del producto: {producto2.nombre}, es de: {producto2.descuento()}")
 
 if __name__ == "__main__":
     main()
