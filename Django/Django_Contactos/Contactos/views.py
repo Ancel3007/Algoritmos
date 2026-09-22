612abc641cdae77f6d8a96d76ae7b2a120471e05
Ancel3007 <aogtte@gmail.com>
2026-09-22 00:51:42 -0500

    Django_Contactos

---
+++ Django/Django_Contactos/Contactos/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Contacto
+
+# Create your views here.
+
+def crear(request):
+    if request.method == POST:
+        contacto = Contacto(
+            nombre = request.POST["nombre"]
+            correo = request.POST["correo"]
+            telefono = request.POST["telefono"]
+            mensaje = request.POST["mensaje"]
+        )
+
+        contacto.save()
+        return render(request, "contactos/formularios.html")
